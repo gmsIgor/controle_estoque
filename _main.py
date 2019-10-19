02cd01c7d7244437225d0de53ad54d829a80696d
hyp <igor.holanda.main@gmail.com>
2019-10-18 22:43:29 -0300

    setup

--- _main.py
+++ _main.py
@@ -7,8 +7,10 @@ def main():
 
     for elto in login.credencial_admin:
         print(credenciais)
-        #print(elto)
-        #print(login.credencial_admin[elto])
+        print(credenciais[0], elto)
+        print(credenciais[1], login.credencial_admin[elto])
+        if credenciais[0].lower == str(elto.lower):
+            print('eita -------------------------')
         if credenciais[0].lower == str(elto.lower) and credenciais[1].lower == str(login.credencial_admin[elto].lower):
             print('deu bom')
             admin = True
