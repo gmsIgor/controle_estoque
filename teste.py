b7b2dbbebbe2c654c4bce7f5075a68d94fa23c83
guei <raphaelvieiraaas@gmail.com>
2019-10-29 08:25:33 -0300

    algumas alterações e função exibir estoque

--- teste.py
+++ teste.py
@@ -2,34 +2,39 @@ def criar_estoque():
     escolha = 1
     estoquedic=[]
     estoque = open("estoque.txt",'a')
+    
     while escolha == 1:
         nome = input('entre com o nome do item  ')
         quantidade = int(input('Entre com a quantidade do item  '))
-        produto = {"Nome:": nome,"quantidade:": quantidade}
+        produto = {"Nome:": nome,"quantidade:": quantidade} 
         escolha = int(input ('digite(1) para continuar editando ou (2) para sair  '))
+        if escolha < 1 or escolha > 2:
+            print('Entre com uma opcao valida')
+            escolha = int(input('digite(1) para continuar editando ou (2)'))
         estoquedic.append(produto) 
-        estoque.writelines(str(produto.items())+ '\n')
-    
-    for i in produto:
-        print(str(produto.values()))
-    return(estoquedic)
-  
-
-
-def editar_estoque(estoquedic):
-    escolha = 1
-    estoquedic=[]
-    estoque = open('estoque.txt','r+')
-    while escolha == 1:
-        nome = input('Entre com o nome do produto  ')
-        quantidade = int(input ('Entre com a quantidade de',nome,':'))
-        produto = {"Nome:": nome,"quantidade:":quantidade}
-        escolha = int(input('Digite(1) para continuar editando e (2) para sair'))
-        estoquedic.append(produto)
-        estoque.writelines(str(produto.items())+'\n')
-    for i in produto: 
-        print(str(produto.values()))
+        estoque.writelines(str(produto.items())+ ';' +'\n')
+      
+    for i in estoquedic:
+        print(i,';','\n')
     return(estoquedic)
 
 
-editar_estoque(estoquedic)
+
+
+
+
+def exibir_estoque():
+    contador = 0
+    estoque = open('estoque.txt','r')
+    for i in estoque:
+        print(i ,';','\n')
+    estoque.close()
+exibir_estoque()    
+
+
+
+
+
+
+
+
