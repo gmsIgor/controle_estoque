069f37c9c6d293ff2f23d7fcf98c1b96ee79d1e0
luistorres909 <lipinhowinho@hotmail.com>
2019-12-04 14:38:01 -0300

    Funções auxiliares para a edição do produto

--- teste.py
+++ teste.py
@@ -1,72 +1,128 @@
-def editar_prod():
-    estoque = open ('estoque.txt','r+')
+def edit_prod():
+    #função que exclui ou altera a quantidade de  um produto.
 
-    estoque.seek(0,0)
+estoque = open ('estoque.txt','r+')
 
-    produtos_list = []
+estoque.seek(0,0)
 
-    estoque_string = estoque.readlines()
+produtos_list = []
 
-    for line in estoque_string :
+estoque_string = estoque.readlines()
 
-        line = line[:-1]
+for line in estoque_string :
 
-        linha = line.split(';') 
+  line = line[:-1]
 
-        produtos_list.append(linha)
+  linha = line.split(';')
+
+  produtos_list.append(linha)
+
+print(produtos_list)
+
+produto = input('Entre com o produto que deseja editar : \n')
+
+
+for contador in range (0,len(produtos_list)) :
+
+  if produtos_list[contador][0] == produto :
+    
+    print('Deseja excluir o produto? Digite 1 para sim e 0 para não.\n')
+    
+    escolha =int(input())
+
+    if escolha == 1:
+
+      produtos_list.pop(contador)
+
+    if escolha == 0:
+
+      print('Entre com a nova quantidade do produto : ')
+      quantidade = input()
+      produtos_list[contador][1] = quantidade
+
+    break
+
+
+
+estoque.close()
+
+
+estoque = open ('estoque.txt','w')
+
+contador = 0
+
+for contador in range (0,len(produtos_list)) :
+
+  estoque.writelines(str(produtos_list[contador][0]) + ';'+ str(produtos_list[contador][1])+'\n')
+
+estoque.close()
+
+
+
+
+
+# função para procurar o  item no estoque , evitando que alguem tente editar um produto que não está no estoque.
+
+def procura_prod(produto)
+
+estoque = open ('estoque.txt','r+')
+
+estoque.seek(0,0)
+
+produtos_list = []
+
+estoque_string = estoque.readlines()
+
+for line in estoque_string :
+
+  line = line[:-1]
+
+  linha = line.split(';')
+
+  produtos_list.append(linha)
+
+#Esse função pode ser separada pois é necessário transformar em lista para fzer as alterações, vou colocar ela logo em seguida.
+
+contador = 0
+produto-em_estoque = 0
+for contador in range (0,len(produtos_list)):
+# Esse produto será um input do produto que deseja procurar.
+    if produtos_list[contador][0] == produto :
+
+        print('Produto está no estoque') #ou nem precisa disso kk
         
+        produto_em_estoque = 1
+        
+        return 1
+
+        break
+
+if produto_em_estoque == 0 :
+
+    print('Produto não se encontra no estoque')
+
+    return 0
+
+#A função retorna 1 se estiver no estoque e 0 caso não esteja.
+....................................................................................................................
+#função que transforma o arquivo numa lista :
+
+def cria_lista(arquivo) #Nem sei se esse argumento é necessario, vejam ai.
 
 
+estoque = open ('estoque.txt','r+')
 
+estoque.seek(0,0)
 
-    print(produtos_list)
+produtos_list = []
 
+estoque_string = estoque.readlines()
 
+for line in estoque_string :
 
-    produto = input('Entre com o produto : \n')
-    for contador in range (0,len(produtos_list)) :
-    
-        if produtos_list[contador][0] == produto :
+  line = line[:-1]
 
-            linha_produto = contador 
+  linha = line.split(';')
 
-            print ('O produto esta na linha' + '\t' + str(contador))
-    
-            
-    
-    linha_remove = int(input('Entre com a linha que deseja excluir\n'))
-    produtos_list.pop(linha_remove)
-    estoque= open('estoquenovo.txt','w')
-    estoque.writelines(str(produtos_list))
-    print(produtos_list)
-    estoque.close()
+  produtos_list.append(linha)
 
-
-
-
-
-
-
-
-
-
-
-
-def editar_prod2():
-    estoque = open('estoque.txt','r+')
-    estoque_string = estoque.readlines()
-    estoque.seek(0,0)
-    
-    for line in estoque_string:
-        line = line[:-1]
-        linha = line.split(';')
-        cont = 0
-        for cont in range(0,len(estoque_string)):
-            Nome =linha[0]
-            Quantidade = linha[1]
-            
-            cont +=1
-    
-        produto = {'Nome:':Nome,'Quantidade:':Quantidade}
-    print (produto)
-editar_prod2()
